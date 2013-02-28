d5fd07202ef2ae8724607a6065d4cfb516abdc16
Patrick <patrick.vargas@colorado.edu>
2013-02-27 17:35:51 -0700

    Deleted my dumb menu system that didn't work.
    Going to look into menu class jkb posted on the chatter.

--- pong/ATLS_4519_Lab3/ATLS_4519_Lab3/Game1.cs
+++ pong/ATLS_4519_Lab3/ATLS_4519_Lab3/Game1.cs
@@ -28,7 +28,6 @@ namespace ATLS_4519_Lab3
         clsSprite ball;
         clsSprite opponent;
         clsSprite player;
-        clsSprite menu;
 
         // SpriteBatch which will draw (render) the sprite
         SpriteBatch spriteBatch;
@@ -58,12 +57,6 @@ namespace ATLS_4519_Lab3
         
         // Define enum for ease of programming
         enum Who { Player, Opponent };
-
-        enum GameState { Menu, Playing, Finish };
-        int selection;
-
-        enum Choice { Demo, Single, Double };
-        int menuPlace;
 
         public Game1()
         {
@@ -103,9 +96,6 @@ namespace ATLS_4519_Lab3
             float ballD = 32; // Ball Diameter
             Random rnd = new Random();
 
-            // Set which state we're in
-            selection = (int) GameState.Menu;
-
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
@@ -140,9 +130,6 @@ namespace ATLS_4519_Lab3
             player   = new clsSprite(Content.Load<Texture2D>("right_paddle"),
                             new Vector2(winX - (10+padX), winY / 2 - padY), new Vector2(padX, padY),
                             winX, winY);
-            menu     = new clsSprite(Content.Load<Texture2D>("small_ball"), 
-                            new Vector2(winX/2 - ballD, winY/2), new Vector2(ballD, ballD),
-                            winX, winY);
             // Create a SpriteBatch to render the sprite
             spriteBatch = new SpriteBatch(graphics.GraphicsDevice);
 
@@ -160,7 +147,6 @@ namespace ATLS_4519_Lab3
             ball.texture.Dispose();
             opponent.texture.Dispose();
             player.texture.Dispose();
-            menu.texture.Dispose();
             spriteBatch.Dispose();
         }
 
@@ -174,114 +160,76 @@ namespace ATLS_4519_Lab3
             // Get User input
             KeyboardState keyboardState = Keyboard.GetState();
 
-            switch (selection) {
-                case (int) GameState.Menu:
-                    int menX = winX / 2, menY = winY / 2;
-                    if (keyboardState.IsKeyDown(Keys.Up))   { menuPlace = (menuPlace + 1) % 3; }
-                    if (keyboardState.IsKeyDown(Keys.Down)) { menuPlace = (menuPlace - 1) % 3; }
+            // Grab midpoint
+            Vector2 midpoint = ball.Midpoint(opponent);
+            float pitch = (midpoint.Y / winY) * 2 - 1;
+            float pan   = (midpoint.X / winX) * 2 - 1;
+            // Add chance to the mix
+            Random rnd = new Random();
+            // Move ball and get information
+            int wall = ball.Move();
+            // Check for kill shot
+            int[] kill = new int [] { 0, 0 };
 
-                    switch (menuPlace) {
-                        case (int)Choice.Demo:
-                            menu.position = new Vector2(menX - menu.size.X, menY + Font1.LineSpacing * 3);
-                            break;
-                        case (int)Choice.Single:
-                            menu.position = new Vector2(menX - menu.size.X, menY + Font1.LineSpacing);
-                            break;
-                        case (int)Choice.Double:
-                            menu.position = new Vector2(menX - menu.size.X, menY + Font1.LineSpacing * 2);
-                            break;
-                        default:
-                            break;
-                    }
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
 
-                    switch (menuPlace) {
-                        case (int) Choice.Demo:
-                            break;
-                        case (int) Choice.Single:
-                            break;
-                        case (int) Choice.Double:
-                            break;
-                        default:
-                            break;
-                    }
-                    break;
-                case (int) GameState.Finish:
-                    break;
-                case (int) GameState.Playing:
-                    // Grab midpoint
-                    Vector2 midpoint = ball.Midpoint(opponent);
-                    float pitch = (midpoint.Y / winY) * 2 - 1;
-                    float pan   = (midpoint.X / winX) * 2 - 1;
-                    // Add chance to the mix
-                    Random rnd = new Random();
-                    // Move ball and get information
-                    int wall = ball.Move();
-                    // Check for kill shot
-                    int[] kill = new int [] { 0, 0 };
-
-                    // Allows the game to exit
-                    if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
-                        this.Exit();
-
-                    // Move the sprite
-                    if (wall > 0) { // Check if hit wall
-                        switch (wall) {
-                            case 2: // Player Miss
-                                threshold -= 10;
-                                ball.position = new Vector2(winX / 2, winY / 2);
-                                ball.velocity = new Vector2(-rnd.Next(3, 6), rnd.Next(-5, 6));
-                                break;
-                            case 3: // Opponent Miss
-                                threshold += 10;
-                                ball.position = new Vector2(winX / 2, winY / 2);
-                                ball.velocity = new Vector2(rnd.Next(3, 6), rnd.Next(-5, 6));
-                                break;
-                            default:
-                                soundEffect1.Play();
-                                break;
-                        }
-                    }
-
-                    // Change the sprite 2 position using the left thumbstick of the Xbox 360 controller
-                    // Vector2 LeftThumb = GamePad.GetState(PlayerIndex.One).ThumbSticks.Left;
-                    // opponent.position += new Vector2(LeftThumb.X, -LeftThumb.Y) * 5;
-                    
-                    //  Change the sprite 2 position using the keyboard
-                    if (keyboardState.IsKeyDown(Keys.Up)) { player.position += new Vector2(0, -5); }
-                    if (keyboardState.IsKeyDown(Keys.Down)) { player.position += new Vector2(0, 5); }
-
-                    // Opponent Algorithm
-                    if (ball.position.X < threshold) {
-                        if (opponent.position.Y > ball.position.Y) { opponent.position += new Vector2(0, -5); }
-                        if (opponent.position.Y < ball.position.Y) { opponent.position += new Vector2(0, 5); }
-                    }
-
-                    if ((kill[(int)Who.Player] = ball.Collides(player)) > 0 ||
-                        (kill[(int)Who.Opponent] = ball.Collides(opponent)) > 0) {
-                            if (kill[(int)Who.Player] == 2 || kill[(int)Who.Opponent] == 2) //Kill Shot!
-                                ball.velocity = new Vector2(-ball.velocity.X + (rnd.Next(-10, 10)), ball.velocity.Y + (rnd.Next(-10, 10)));
-                            else
-                                ball.velocity = new Vector2(-ball.velocity.X + (rnd.Next(-2, 2)), ball.velocity.Y + (rnd.Next(-2, 2)));
-                        GamePad.SetVibration(PlayerIndex.One, 1.0f, 1.0f);
-
-                        //soundEffect2.Play(1.0f, pitch, pan);
-                        //soundEffect1.Play();
-            
-                        // Get an instance of the cue from the XACT project
-                        //cue = soundBank.GetCue("Explosion");
-                        //cue.Play();
-                    }
-                    else
-                        GamePad.SetVibration(PlayerIndex.One, 0f, 0f);
-
-                    // Update the audio engine
-                    audioEngine.Update();
-
-                    base.Update(gameTime);
-                    break;
-                default:
-                    break;
+            // Move the sprite
+            if (wall > 0) { // Check if hit wall
+                switch (wall) {
+                    case 2: // Player Miss
+                        threshold -= 10;
+                        ball.position = new Vector2(winX / 2, winY / 2);
+                        ball.velocity = new Vector2(-rnd.Next(3, 6), rnd.Next(-5, 6));
+                        break;
+                    case 3: // Opponent Miss
+                        threshold += 10;
+                        ball.position = new Vector2(winX / 2, winY / 2);
+                        ball.velocity = new Vector2(rnd.Next(3, 6), rnd.Next(-5, 6));
+                        break;
+                    default:
+                        soundEffect1.Play();
+                        break;
+                }
             }
+
+            // Change the sprite 2 position using the left thumbstick of the Xbox 360 controller
+            // Vector2 LeftThumb = GamePad.GetState(PlayerIndex.One).ThumbSticks.Left;
+            // opponent.position += new Vector2(LeftThumb.X, -LeftThumb.Y) * 5;
+            
+            //  Change the sprite 2 position using the keyboard
+            if (keyboardState.IsKeyDown(Keys.Up)) { player.position += new Vector2(0, -5); }
+            if (keyboardState.IsKeyDown(Keys.Down)) { player.position += new Vector2(0, 5); }
+
+            // Opponent Algorithm
+            if (ball.position.X < threshold) {
+                if (opponent.position.Y > ball.position.Y) { opponent.position += new Vector2(0, -5); }
+                if (opponent.position.Y < ball.position.Y) { opponent.position += new Vector2(0, 5); }
+            }
+
+            if ((kill[(int)Who.Player] = ball.Collides(player)) > 0 ||
+                (kill[(int)Who.Opponent] = ball.Collides(opponent)) > 0) {
+                    if (kill[(int)Who.Player] == 2 || kill[(int)Who.Opponent] == 2) //Kill Shot!
+                        ball.velocity = new Vector2(-ball.velocity.X + (rnd.Next(-10, 10)), ball.velocity.Y + (rnd.Next(-10, 10)));
+                    else
+                        ball.velocity = new Vector2(-ball.velocity.X + (rnd.Next(-2, 2)), ball.velocity.Y + (rnd.Next(-2, 2)));
+                GamePad.SetVibration(PlayerIndex.One, 1.0f, 1.0f);
+
+                //soundEffect2.Play(1.0f, pitch, pan);
+                //soundEffect1.Play();
+    
+                // Get an instance of the cue from the XACT project
+                //cue = soundBank.GetCue("Explosion");
+                //cue.Play();
+            }
+            else
+                GamePad.SetVibration(PlayerIndex.One, 0f, 0f);
+
+            // Update the audio engine
+            audioEngine.Update();
+
+            base.Update(gameTime);
         }
 
         /// <summary>
@@ -296,32 +244,16 @@ namespace ATLS_4519_Lab3
             // In 4.0, this behavior is the default; in XNA 3.1, it is not
             spriteBatch.Begin();
 
-            switch (selection) {
-                case (int)GameState.Menu:
-                    spriteBatch.DrawString(Font1, "Demo", new Vector2(winX / 2, winY / 2), Color.Yellow);
-                    spriteBatch.DrawString(Font1, "Single Player", new Vector2(winX / 2, winY / 2 + Font1.LineSpacing), Color.Yellow);
-                    spriteBatch.DrawString(Font1, "Two Player", new Vector2(winX / 2, winY / 2 + Font1.LineSpacing*2), Color.Yellow);
+            // Write Scores
+            spriteBatch.DrawString(Font1, "Opponent: " + ball.score[(int)Who.Opponent], new Vector2(5, 10), Color.Yellow);
+            spriteBatch.DrawString(Font1, "Player: " + ball.score[(int)Who.Player], new Vector2(winX - Font1.MeasureString("Player: " + ball.score[0]).X - 5, 10), Color.Yellow);
+            spriteBatch.DrawString(Font1, "Vel: ( " + ball.velocity.X + ", " + ball.velocity.Y + " ) Threshold: " + threshold, new Vector2(5, winY - Font1.LineSpacing), Color.Yellow);
 
-                    menu.Draw(spriteBatch);
-                    
-                    break;
-                case (int)GameState.Finish:
-                    break;
-                case (int)GameState.Playing:
-                    // Write Scores
-                    spriteBatch.DrawString(Font1, "Opponent: " + ball.score[(int)Who.Opponent], new Vector2(5, 10), Color.Yellow);
-                    spriteBatch.DrawString(Font1, "Player: " + ball.score[(int)Who.Player], new Vector2(winX - Font1.MeasureString("Player: " + ball.score[0]).X - 5, 10), Color.Yellow);
-                    spriteBatch.DrawString(Font1, "Vel: ( " + ball.velocity.X + ", " + ball.velocity.Y + " ) Threshold: " + threshold, new Vector2(5, winY - Font1.LineSpacing), Color.Yellow);
-
-                    // Draw physical objects
-                    ball.Draw(spriteBatch);
-                    opponent.Draw(spriteBatch);
-                    player.Draw(spriteBatch);
-                    break;
-                default:
-                    break;
-            }
-
+            // Draw physical objects
+            ball.Draw(spriteBatch);
+            opponent.Draw(spriteBatch);
+            player.Draw(spriteBatch);
+    
             spriteBatch.End();
 
             base.Draw(gameTime);
